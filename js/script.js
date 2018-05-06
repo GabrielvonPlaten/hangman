31a605f8453da70a68691a61e3ebc0b5e9669ac1
Gabriel von Platen <gabrielvonplaten@gmail.com>
2018-05-07 00:10:13 +0200

    change var to let

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Globala variabler
 window.onload = init;
-var wordList = ['DEVELOPER', 'MICROSOFT', 'GALAXY', 'SILICON'];
+let wordList = ['DEVELOPER', 'MICROSOFT', 'GALAXY', 'SILICON'];
 
-var correctAnswerNumber = 0;
-var hangmanImg;
-var hangmanImgNumber = 0;
+let correctAnswerNumber = 0;
+let hangmanImg;
+let hangmanImgNumber = 0;
 
 
 //Restart game Button
@@ -14,22 +14,22 @@ function restartGameButton() {
 
 function init() {
     //Start game button
-    var startGameButton = document.getElementById('startGameBtn');
+    let startGameButton = document.getElementById('startGameBtn');
 
-    var letterButtons = document.getElementById('letterButtons');
-    var alphabetBtns = document.querySelectorAll('.btn');
+    let letterButtons = document.getElementById('letterButtons');
+    let alphabetBtns = document.querySelectorAll('.btn');
 
     //Timer variables
-    var timerSeconds = document.getElementById('seconds');
-    var timerDecimals = document.getElementById('decimals');
-    var interval;
+    let timerSeconds = document.getElementById('seconds');
+    let timerDecimals = document.getElementById('decimals');
+    let interval;
 
 
     //Start function
     startGameButton.addEventListener('click', startGameFunction, false);
 
     function startGameFunction(event) {
-        for (var i = 0; i < alphabetBtns.length; i++) {
+        for (let i = 0; i < alphabetBtns.length; i++) {
             alphabetBtns[i].removeAttribute('disabled');
         }
         document.getElementById('time').style.display= 'block';
@@ -44,15 +44,15 @@ function init() {
 
     //The function which generates the letter boxes
     function letterBoxes() {
-        for (var i = 0; i < selectedWord.length; i++) {
-            var box = document.createElement('li');
+        for (let i = 0; i < selectedWord.length; i++) {
+            let box = document.createElement('li');
             box.innerHTML = '<input disabled id=boxList' + i + '>';
             letterBoxesUl.appendChild(box);
         }
     }
 
     //This will send the answer to checkAnswer once a letter button is cliced.
-    for (var j = 0; j < alphabetBtns.length; j++) {
+    for (let j = 0; j < alphabetBtns.length; j++) {
         alphabetBtns[j].addEventListener('click', checkAnswer);
         alphabetBtns[j].addEventListener('click', function (event) {
             event.target.setAttribute('disabled', 'disabled');
@@ -61,7 +61,7 @@ function init() {
 
     //Check if your answer is correct
     function checkAnswer(event) {
-        var answer = event.target.value;
+        let answer = event.target.value;
         if (selectedWord.indexOf(answer) >= 0) {
             correctAnswer(answer);
         } else {
@@ -71,7 +71,7 @@ function init() {
 
     //Correct answer & winning function
     function correctAnswer(answer) {
-        for (var j = 0; j < selectedWord.length; j++) {
+        for (let j = 0; j < selectedWord.length; j++) {
             if (selectedWord[j] == answer) {
                 document.getElementById('boxList' + j++).value = answer;
                 correctAnswerNumber++;
@@ -89,11 +89,11 @@ function init() {
     //Wrong answer function
     function incorrectAnswer() {
         hangmanImgNumber++;
-        var hangmanImg = document.getElementById('hangman');
+        let hangmanImg = document.getElementById('hangman');
         hangmanImg.src = 'images/h' + hangmanImgNumber + '.png';
         //Disables all buttons
         if (hangmanImgNumber === 6) {
-            for (var f = 0; f < alphabetBtns.length; f++) {
+            for (let f = 0; f < alphabetBtns.length; f++) {
                 alphabetBtns[f].setAttribute('disabled', 'disabled');
             }
             //Message after guessing all wrong
@@ -105,18 +105,18 @@ function init() {
 
     //Game timer. Once it reaches 0 the game ends.
     function Timer() {
-        var timeleft = 30;
-        var downloadTimer = setInterval(function () {
+        let timeleft = 30;
+        let downloadTimer = setInterval(function () {
             timeleft--;
             document.getElementById("countdowntimer").textContent = timeleft;
             //Disables all buttons
             if (timeleft <= 0) {
-                for (var f = 0; f < alphabetBtns.length; f++) {
+                for (let f = 0; f < alphabetBtns.length; f++) {
                     alphabetBtns[f].setAttribute('disabled', 'disabled');
                 }
                 //Message after it reaches 0
                 clearInterval(downloadTimer);
-                var hangmanImg = document.getElementById('hangman');
+                let hangmanImg = document.getElementById('hangman');
                 hangmanImg.src = 'images/h6.png';
                 document.getElementById('message').innerHTML = 'You have died. Bitter and alone.';
                 document.getElementById('time').innerText = 'The correct word was ' + selectedWord;
